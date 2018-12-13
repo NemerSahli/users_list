667aaaa35bff2f47eeeae1223d72108847ab4e41
Nemer EL-Sahli <nemer.sahli@gmx.de>
2018-12-13 07:30:44 +0100

    actions.js remove the unusefull functions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,44 +1,5 @@
 import axios from 'axios';
 
-export const logOut = () => async dispatch => {
-  try {
-    const res = await axios('http://localhost:8000/logout', {
-      method: 'post',
-      withCredentials: true
-    });
-
-    console.log(res);
-    if (res.data.error === 0) {
-      // alert('no error');
-      dispatch({ type: 'LOGOUT' });
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-export const signUp = (signUpUser, routeTo) => async dispatch => {
-  alert(
-    'signUp actions' + JSON.stringify(signUpUser) + JSON.stringify(routeTo)
-  );
-  try {
-    const result = await axios('http://localhost:8000/signup', {
-      method: 'post',
-      data: signUpUser,
-      withCredentials: true
-    });
-    console.log(result);
-    if (result.data.error === 0) {
-      dispatch({ type: 'SIGN_UP_FAILD', error: '' });
-      routeTo.push('/signupsuccessful');
-    } else {
-      dispatch({ type: 'SIGN_UP_FAILD', error: result.data.message });
-    }
-  } catch (e) {
-    alert('error:' + e);
-  }
-};
-
 export const loadUsers = () => async dispatch => {
   try {
     const result = await axios('http://localhost:8000/users', {
